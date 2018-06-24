74dab2d3434b516465caf906f4e919ae8962c3bf
mcculov <alexeynm@bk.ru>
2018-06-24 15:08:00 +0300

    Initial commit
    Signed-off-by: mcculov <alexeynm@bk.ru>

---
+++ app/src/main/java/com/example/mcculov/lingphone/LingActivity.java
@@ -0,0 +1,10 @@
+package com.example.mcculov.lingphone;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class LingActivity extends Activity {
+    public static final String LING_PREFERENCES = "LingPrefs";
+}
+
+
---
+++ app/src/main/java/com/example/mcculov/lingphone/LingAppCompatActivity.java
@@ -0,0 +1,8 @@
+package com.example.mcculov.lingphone;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class LingAppCompatActivity extends AppCompatActivity {
+    public static final String LING_PREFERENCES = "LingPrefs";
+}
